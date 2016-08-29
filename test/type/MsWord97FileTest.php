4640e015ae4cab55080e1d59c1db10cfe6096765
jaberu <alexander.herr.81@gmail.com>
2016-08-29 18:49:29 +0200

    uninitialized offset issue

--- test/type/MsWord97FileTest.php
+++ test/type/MsWord97FileTest.php
@@ -30,4 +30,21 @@ class MsWord97FileTest extends \PHPUnit_Framework_TestCase {
         // middle of a word why we actually have one word more. That's a pitty ...
         $this->assertEquals(36, $cnt);
     }
+    
+    /**
+     * Related to a bug already reported here:
+     * https://github.com/PHPOffice/PHPWord/issues/853
+     * 
+     * As the document is also not processed correctly an error is exected.
+     * However we cannot override the system error handler to rethrown that
+     * issue as an exception since the PhpWord Project also uses the @ operator
+     * at some points. If we replace the native error handler even more places
+     * would raise an error.     * 
+     * 
+     * @expectedException PHPUnit_Framework_Error_Notice 
+     */
+    public function testUninitializedOffset() {
+        $uninitOffset = new MsWord97File(__DIR__ . '/../resources/uninitialized_offset.doc');
+        $uninitOffset->getWordCount();
+    }
 }
